96c0079a0efeca8c3ffa196c245dfe61ac23b576
QUI NGUYEN <qui@mit.edu>
2014-02-28 15:55:03 -0500

    make quotes uniform and add some global params

--- predictionio.js
+++ predictionio.js
@@ -9,48 +9,54 @@
 var predictionio = function (app_key, options) {
 
   var APP_KEY,
-    HOST = '';
+    HOST = '',
+    USERS_ENDPOINT = 'users',
+    USER_PARAMS = ['pio_uid', 'pio_latlng', 'pio_inactive'];
 
   if (!app_key) {
-      throw new Error("An app key is required to use the API.");
+      throw new Error('An app key is required to use the API.');
   }
   APP_KEY = app_key;
-  if ( (options !== null) && (typeof options.host !== "undefined") ) {
+  if ( (options !== null) && (typeof options.host !== 'undefined') ) {
     HOST = options.host;
   }
 
   // Makes an AJAX request to the specified endpoint, with the given method and data,
   // and then calls the callback on the server's response.
   var make_request = function (endpoint, method, data, callback) {
+    // TODO
     return true;
   }
 
   // Adds a user, specified by the user_info JSON object, then calls the callback
   // function on the server's response.
   var add_user = function (user_info, callback) {
-    if (typeof user_info.pio_uid === "undefined") {
-      throw new Error("To add a user, you must specify the user's pio_uid.");
-      // cannot contain \t or ,. How much error checking to do?
+    if (typeof user_info.pio_uid === 'undefined"' {
+      throw new Error('To add a user, you must specify the user's pio_uid.'');
+      // pio_uid cannot contain \t or ,. How much error checking to do?
     }
+
+    // Copy the user_info and delete any unauthorized params.
     var data = JSON.parse(JSON.stringify(user_info));
     for(var propertyName in data) {
-      if (propertyName.slice(0,4) === "pio_" && (!param_allowed(USER_PARAMS, propertyName))) {
+      if (propertyName.slice(0,4) === 'pio_' && (!param_allowed(USER_PARAMS, propertyName))) {
         delete data[propertyName];
         // Log warning?
       }
     }
-    make_request('users.json', 'POST', data, callback);
+
+    make_request(USERS_ENDPOINT.concat('.json'), 'POST', data, callback);
   }
 
   // Gets a user.
   var get_user = function(pio_uid, callback) {
-    var endpoint = 'users/'.concat(pio_uid).concat('.json');
+    var endpoint = USERS_ENDPOINT.concat('/', pio_uid, '.json');
     make_request(endpoint, 'GET', callback);
   }
 
   // Deletes a user.
   var delete_user = function(pio_uid, callback) {
-    var endpoint = 'users/'.concat(pio_uid).concat('.json');
+    var endpoint = USERS_ENDPOINT.concat('/', pio_uid, '.json');
     make_request(endpoint, 'DELETE', callback);
   }
 
