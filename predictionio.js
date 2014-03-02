11ad4ae09120dccac766c2692b416360ff4e1376
QUI NGUYEN <qui@mit.edu>
2014-03-02 16:08:13 -0500

    Filled in make_request

--- predictionio.js
+++ predictionio.js
@@ -10,7 +10,7 @@ var predictionio = function (app_key, options) {
 
   var pub = {},
     APP_KEY,
-    HOST = '',
+    HOST = 'http://localhost:8000/',
     USERS_ENDPOINT = 'users';
 
   if (!app_key) {
@@ -18,14 +18,45 @@ var predictionio = function (app_key, options) {
   }
   APP_KEY = app_key;
   if ( (typeof options !== 'undefined') && (typeof options.host !== 'undefined') ) {
-    HOST = options.host;
+    HOST = options.host; // Check if their provided host ends in a slash
   }
 
   // Makes an AJAX request to the specified endpoint, with the given method and data,
   // and then calls the callback on the server's response.
-  var make_request = function (endpoint, method, data, callback) {
-    // TODO
-    return true;
+  var make_request = function (url, method, data, callback) {
+
+    var encode_data = function(obj) {
+      var str = [];
+      for(var p in obj)
+        if (obj.hasOwnProperty(p)) {
+          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+      str.push("pio_appkey=" + APP_KEY);
+      return str.join("&");
+    }
+
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function() {
+      if(request.readyState == 4 && request.status == 200) {
+        var response_obj = JSON.parse(request.responseText);
+        callback(response_obj);
+      }
+    }
+
+    if (method === "GET" || method === "DELETE") {
+      var final_url = HOST.concat(url, "?", encode_data(data));
+      request.open(method, final_url);
+      request.send();
+    }
+    else if (method === "POST" || method === "PUT") {
+      request.open(method, HOST.concat(url));
+      request.setRequestHeader("Content-type", "x-www-form-urlencoded");
+      request.send(encode_data(data));
+    }
+    else {
+      throw new Error("Invalid HTTP method: ".concat(method));
+    }
   };
 
   // Adds a user, specified by the user_info JSON object, then calls the callback
@@ -43,13 +74,13 @@ var predictionio = function (app_key, options) {
   // Gets a user.
   pub.get_user = function(pio_uid, callback) {
     var endpoint = USERS_ENDPOINT.concat('/', pio_uid, '.json');
-    make_request(endpoint, 'GET', callback);
+    make_request(endpoint, 'GET', {}, callback);
   }
 
   // Deletes a user.
   pub.delete_user = function(pio_uid, callback) {
     var endpoint = USERS_ENDPOINT.concat('/', pio_uid, '.json');
-    make_request(endpoint, 'DELETE', callback);
+    make_request(endpoint, 'DELETE', {}, callback);
   }
 
   return pub;
--- tests.js
+++ tests.js
@@ -1,4 +1,12 @@
 describe("pdio suite", function() {
+
+  var check_response = function(expected) {
+    var func = function(response) {
+      expect(response).toEqual(expected);
+    };
+    return func;
+  };
+
   it("Initialization", function() {
     expect(predictionio('app_key')).not.toBeNull();
     expect(function() {
@@ -8,24 +16,23 @@ describe("pdio suite", function() {
   });
 
   it("Add user", function() {
-    client = predictionio('app_key');
-    expect(function() {
-      client.add_user({'pio_uid': 'user_id'});
-      }).not.toThrow();
+    var client = predictionio('app_key');
+    var expected = {url: "http://localhost:8000/users.json", header: "x-www-form-urlencoded",
+                    data: "pio_uid=user_id&pio_appkey=app_key", method: "POST"};
+    client.add_user({'pio_uid': 'user_id'}, check_response(expected));
   });
 
   it("Get user", function() {
-    client = predictionio('app_key');
-    expect(function() {
-      client.get_user('user_id');
-      }).not.toThrow();
+    var client = predictionio('app_key');
+    var expected = {url: "http://localhost:8000/users/user_id.json?pio_appkey=app_key", method: "GET"};
+    client.get_user('user_id', check_response(expected));
   });
 
   it("Delete user", function() {
     client = predictionio('app_key');
-    expect(function() {
-      client.delete_user('user_id');
-      }).not.toThrow();
+    var expected = {url: "http://localhost:8000/users/user_id.json?pio_appkey=app_key", method: "DELETE"};
+    client.delete_user('user_id', check_response(expected));
   });
 
 });
+
