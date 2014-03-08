23acfb54ddd5719bb4f492fffcd94670e3ad2e76
QUI NGUYEN <qui@mit.edu>
2014-03-07 23:03:43 -0500

    Items and Actions APIs implemented

--- predictionio.js
+++ predictionio.js
@@ -11,7 +11,9 @@ var predictionio = function (app_key, options) {
   var pub = {},
     APP_KEY,
     HOST = 'http://localhost:8000/',
-    USERS_ENDPOINT = 'users';
+    USERS_ENDPOINT = 'users',
+    ITEMS_ENDPOINT = 'items',
+    ACTIONS_ENDPOINT = 'actions';
 
   if (!app_key) {
       throw new Error('An app key is required to use the API.');
@@ -25,7 +27,7 @@ var predictionio = function (app_key, options) {
   // and then calls the callback on the server's response.
   var make_request = function (url, method, data, callback) {
 
-    var encode_data = function(obj) {
+    var encode_data = function (obj) {
       var str = [];
       for(var p in obj)
         if (obj.hasOwnProperty(p)) {
@@ -33,16 +35,18 @@ var predictionio = function (app_key, options) {
         }
       str.push("pio_appkey=" + APP_KEY);
       return str.join("&");
-    }
+    };
 
     var request = new XMLHttpRequest();
 
-    request.onreadystatechange = function() {
-      if(request.readyState == 4 && Math.floor(request.status/100) == 2) {
-        var response_obj = JSON.parse(request.responseText);
-        callback(response_obj);
-      }
-      // Adding a user returns 201, not 200. Better way to check for success?
+    if (typeof callback !== 'undefined') {
+      request.onreadystatechange = function () {
+        if(request.readyState == 4 && Math.floor(request.status/100) == 2) {
+          var response_obj = JSON.parse(request.responseText);
+          callback(response_obj);
+        }
+        // Better way to check for success?
+      };
     };
 
     if (method === "GET" || method === "DELETE") {
@@ -60,29 +64,68 @@ var predictionio = function (app_key, options) {
     }
   };
 
+  // Helper function that throws an error unless all the required params in the given array are
+  // defined in the given object.
+  var check_required_params = function (object, required_params, method_name) {
+    var missing_params = [];
+    for (var i = 0; i < required_params.length; i++) {
+      if (typeof object[required_params[i]] === 'undefined') {
+        missing_params.push(required_params[i]);
+      }
+    }
+    if (missing_params.length > 0) {
+      var missing_string = missing_params.join(', ');
+      throw new Error("The following required parameters for ".concat(method_name, " are missing: ", missing_string));
+    }
+  };
+
   // Adds a user, specified by the user_info JSON object, then calls the callback
   // function on the server's response.
   pub.add_user = function (user_info, callback) {
-    if (typeof user_info.pio_uid === 'undefined') {
-      throw new Error('To add a user, you must specify the user\'s pio_uid.');
-      // pio_uid cannot contain \t or ,. How much error checking to do?
-    }
-
+    check_required_params(user_info, ['pio_uid'], 'add_user');
     // Check the user_info for illegal keys, etc?
     make_request(USERS_ENDPOINT.concat('.json'), 'POST', user_info, callback);
-  }
+  };
 
   // Gets a user.
-  pub.get_user = function(pio_uid, callback) {
+  pub.get_user = function (pio_uid, callback) {
     var endpoint = USERS_ENDPOINT.concat('/', pio_uid, '.json');
     make_request(endpoint, 'GET', {}, callback);
-  }
+  };
 
   // Deletes a user.
-  pub.delete_user = function(pio_uid, callback) {
+  pub.delete_user = function (pio_uid, callback) {
     var endpoint = USERS_ENDPOINT.concat('/', pio_uid, '.json');
     make_request(endpoint, 'DELETE', {}, callback);
-  }
+  };
+
+  // Adds an item, specified by the item_info JSON object, then calls the callback
+  // function on the server's response.
+  pub.add_item = function (item_info, callback) {
+    check_required_params(item_info, ['pio_iid', 'pio_itypes'], 'add_item');
+    make_request(ITEMS_ENDPOINT.concat('.json'), 'POST', item_info, callback);
+  };
+
+  // Gets an item.
+  pub.get_item = function (pio_iid, callback) {
+    var endpoint = ITEMS_ENDPOINT.concat('/', pio_iid, '.json');
+    make_request(endpoint, 'GET', {}, callback);
+  };
+
+  // Deletes an item.
+  pub.delete_item = function (pio_iid, callback) {
+    var endpoint = ITEMS_ENDPOINT.concat('/', pio_iid, '.json');
+    make_request(endpoint, 'DELETE', {}, callback)
+  };
+
+  // Record a user-to-item action.
+  pub.record = function (action_info) {
+    check_required_params(action_info, ['pio_uid', 'pio_iid', 'pio_action'], 'record');
+    if (action_info.pio_action === "rate") {
+      throw new Error("For record, if pio_action is rate, pio_rate is required.");
+    }
+    make_request(ACTIONS_ENDPOINT.concat('/u2i.json'), 'POST', {});
+  };
 
   return pub;
 }
--- testing/tests.js
+++ testing/tests.js
@@ -12,7 +12,7 @@ describe("Basic initialization tests", function() {
   });
 });
 
-describe("User API tests", function() {
+describe("User API and general tests", function() {
 
   var client = predictionio("app_key", {"host": ""}),
       server,
@@ -41,11 +41,24 @@ describe("User API tests", function() {
   });
   // How to check that POST body is being sent properly?
 
+  it("Add user without uid", function() {
+    expect(function () {
+      client.add_user({'other_key': 'value'}, callback);
+    }).toThrowError();
+  });
+
   it("Verify default host", function() {
     var client_with_default = predictionio("app_key");
     client_with_default.add_user({'pio_uid': 'user_id'});
     expect(server.requests[0].url).toBe("http://localhost:8000/users.json");
     server.respond();
+  });
+
+  it("Works if user does not specify callback", function () {
+    expect(function () {
+      client.add_user({'pio_uid': 'user_id'});
+      server.respond();
+    }).not.toThrowError();
   });
 
   it("Get user", function() {
@@ -64,4 +77,94 @@ describe("User API tests", function() {
   });
 
 });
+
+describe("Item API tests", function() {
+
+  var client = predictionio("app_key", {"host": ""}),
+      server,
+      callback;
+
+  beforeEach(function() {
+    server = sinon.fakeServer.create();
+    server.respondWith("POST", "items.json", [201, {}, '{"message": "Item created."}']);
+    server.respondWith("GET", /^items\/([^\t,]+)\.json\?pio_appkey\=app_key$/, function(xhr, iid) {
+      xhr.respond(200, {}, JSON.stringify({"pio_iid": iid}));
+    });
+    server.respondWith("DELETE", /^items\/([^\t,]+)\.json\?pio_appkey\=app_key$/, '{"message": "Item deleted."}');
+      // need to check what status code is returned by the actual server
+    callback = sinon.spy();
+  });
+
+  afterEach(function() {
+    server.restore();
+    callback.reset();
+  });
+
+  it("Add item", function() {
+    client.add_item({'pio_iid': 'item_id', 'pio_itypes': 'type1,type2'}, callback);
+    server.respond();
+    expect(callback.calledOnce).toBe(true);
+  });
+
+  it("Add item, missing item_iid or itypes", function() {
+    expect(function () {
+      client.add_item({'other_key': 'value'}, callback);
+    }).toThrowError();
+
+    expect(function () {
+      client.add_item({'item_iid': 'item_id'}, callback);
+    }).toThrowError();
+  });
+
+  it("Get item", function() {
+    client.get_item("item200", callback);
+    server.respond();
+    expect(callback.calledOnce).toBe(true);
+    expect(callback.firstCall.args.length).toBe(1);
+    expect(callback.firstCall.args[0].pio_iid).toBeDefined();
+    expect(callback.firstCall.args[0].pio_iid).toBe("item200");
+  });
+
+  it("Delete item", function() {
+    client.delete_item({'pio_iid': 'item_id'}, callback);
+    server.respond();
+    expect(callback.calledOnce).toBe(true);
+  });
+
+});
+
+describe("Action API tests", function () {
+
+  var client = predictionio("app_key", {"host": ""}),
+      server,
+      callback;
+
+  beforeEach(function() {
+    server = sinon.fakeServer.create();
+    callback = sinon.spy();
+  });
+
+  afterEach(function() {
+    server.restore();
+    callback.reset();
+  });
+
+  it("Record action, missing param(s)", function () {
+    expect(function () {
+      client.record({'pio_uid': 'uid', 'pio_iid': 'id'});
+    }).toThrowError();
+    expect(function () {
+      client.record({'pio_uid': 'uid', 'pio_action': 'like'});
+    }).toThrowError();
+    expect(function () {
+      client.record({'pio_action': 'dislike', 'pio_iid': 'id'});
+    }).toThrowError();
+    expect(function () {
+      client.record({'pio_uid': 'uid', 'pio_iid': 'id', 'pio_action': 'rate'});
+    }).toThrowError();
+  });
+
+});
+
+
 
